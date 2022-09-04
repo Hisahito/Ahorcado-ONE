0cf76f5096308492763e2337901d82999875c626
hisahito <hisahito706@gmail.com>
2022-09-04 01:48:06 -0500

    fixing some bugs, changing some tags and names, adding new methods

--- script.js
+++ script.js
@@ -72,6 +72,7 @@ let Guess = (choosenLetter) => {
     document.getElementById(choosenLetter).style.color = 'grey';
 
     if(answer.indexOf(choosenLetter) >= 0) {
+        console.log(answer)
         wordGuess();
         GameWon();
     }else if (answer.indexOf(choosenLetter) === -1) {
@@ -80,15 +81,15 @@ let Guess = (choosenLetter) => {
         GameLost();
         hangmanUpdate();
     }
-
 }
 
 let GameWon = () => {
     if(wordStatus === answer) {
+        console.log(answer)
         resultBox.style.display = 'block';
         message.innerHTML = 'Congratulations! You Won!';
         message.style.color = 'green';
-        result_box_answer.innerHTML = 'The answer is: ' + answer;
+        result_box_answer.innerHTML = answer;
     }
 }
 
@@ -97,12 +98,65 @@ let GameLost = () => {
         resultBox.style.display = 'block';
         message.innerHTML = 'You Lost!';
         message.style.color = 'red';
-        result_box_answer.innerHTML = 'The answer is: ' + answer;
+        result_box_answer.innerHTML = answer;
     }
 }
 
 let wordGuess = () => {
     wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-    document.querySelector('.wordSpotLight').innerHTML = wordStatus;
+    document.querySelector('.wordSpotlight').innerHTML = wordStatus;
 }
 
+let hangmanUpdate = () => {
+    switch(mistake) {
+        case 1:
+            document.querySelector('.face').style.display = 'block';
+        break;
+        case 2:
+            document.querySelector('.stomach').style.display = 'block';
+        break;
+        case 3:
+            document.querySelector('.hand-one').style.display = 'block';
+        break;
+        case 4:
+            document.querySelector('.hand-two').style.display = 'block';
+        break;
+        case 5:
+            document.querySelector('#leg-one').style.display = 'block';
+        break;
+        case 6:
+            document.querySelector('#leg-two').style.display = 'block';
+        break;
+    }
+}
+
+let reset = () => {
+    mistake = 0;
+    guessed = [];
+    RandomWord();
+    wordGuess();
+    mistakeUpdate();
+    hangmanUpdate();
+    generateButtons();
+    document.querySelector('.face').style.display = 'none';
+    document.querySelector('.stomach').style.display = 'none';
+    document.querySelector('.hand-one').style.display = 'none';
+    document.querySelector('.hand-two').style.display = 'none';
+    document.querySelector('#leg-one').style.display = 'none';
+    document.querySelector('#leg-two').style.display = 'none';
+}
+
+let newGame = () => {
+    resultBox.style.display = 'none';
+    reset();
+}
+
+let mistakeUpdate = () => {
+    document.querySelector('.mistake').innerHTML = mistake;
+}
+
+max_wrong.innerHTML = maxWrong;
+
+RandomWord();
+generateButtons();
+wordGuess();
